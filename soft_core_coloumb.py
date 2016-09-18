b864123de933b615988eef650c20c3db025757af
dbondar <kdfstudio@gmail.com>
2016-09-17 22:22:37 -0400

    soft core columb potential

--- soft_core_coloumb.py
+++ soft_core_coloumb.py
@@ -1,3 +1,110 @@
+__doc__ = """
+To check accuracy of FastWigner vs AccurateWigner propagator
+"""
+
+import matplotlib.pyplot as plt
+
+# Import propagators
+from FastWigner.wigner_bloch_cuda_1d import WignerBlochCUDA1D as FastWignerPropagator
+from FastWigner.wigner_normalize import WignerNormalize, WignerSymLogNorm
+from AccurateWigner.wigner_bloch_cuda_1d import WignerBlochCUDA1D as AccurateWignerPropagator
+
+##########################################################################################
+#
+#   Parameters of quantum systems
+#
+##########################################################################################
+sys_params = dict(
+    t=0.,
+    dt=0.01,
+
+    X_gridDIM=1024,
+    X_amplitude=30.,
+
+    P_gridDIM=1024,
+    P_amplitude=40.,
+
+    # the kinetic energy
+    K="0.5 * P * P",
+
+    # the soft core Coulomb potential for Ar
+    V="-1. / sqrt(X * X + 1.37)",
+)
+
+##########################################################################################
+#
+#   Parameters of plotting
+#
+##########################################################################################
+
+acc_prop = AccurateWignerPropagator(**sys_params)
+acc_ground_state = acc_prop.get_ground_state(abs_tol_purity=1e-10).get().real
+acc_propagated_state = acc_prop.propagate(100000).get().real
+
+fast_prop = FastWignerPropagator(**sys_params)
+fast_ground_state = fast_prop.get_ground_state(abs_tol_purity=1e-10).get()
+fast_propagated_state = fast_prop.propagate(100000).get()
+
+##########################################################################################
+#
+#   Plotting Fast Propagator
+#
+##########################################################################################
+
+# plotting params
+img_params = dict(
+    extent=[fast_prop.X.min(), fast_prop.X.max(), fast_prop.P.min(), fast_prop.P.max()],
+    origin='lower',
+    aspect=1,
+    cmap='bwr',
+    norm=WignerNormalize(vmin=-5e-8, vmax=5e-8),
+)
+
+plt.subplot(221)
+#plt.title("Ground state Wigner function obtained via Fast Wigner propagator")
+plt.text(-18, -8, '(a)', color='k', fontsize=15)
+plt.imshow(fast_ground_state, **img_params)
+plt.xlim([-20., 20])
+plt.ylim([-10., 10])
+#plt.xlabel('$x$ (a.u.)')
+plt.ylabel('$p$ (a.u.)')
+
+plt.subplot(222)
+#plt.title("Propagated state via Fast Wigner propagator")
+plt.text(-18, -8, '(b)', color='k', fontsize=15)
+plt.imshow(fast_propagated_state, **img_params)
+plt.xlim([-20., 20])
+plt.ylim([-10., 10])
+#plt.xlabel('$x$ (a.u.)')
+#plt.ylabel('$p$ (a.u.)')
+
+##########################################################################################
+#
+#   Plotting Accurate Propagator
+#
+##########################################################################################
+
+# just change the grid parameters
+img_params["extent"]=[acc_prop.X.min(), acc_prop.X.max(), acc_prop.P.min(), acc_prop.P.max()]
+
+plt.subplot(223)
+#plt.title("Ground state Wigner function obtained via Accurate Wigner propagator")
+plt.text(-18, -8, '(c)', color='k', fontsize=15)
+plt.imshow(acc_ground_state, **img_params)
+plt.xlim([-20., 20])
+plt.ylim([-10., 10])
+plt.xlabel('$x$ (a.u.)')
+plt.ylabel('$p$ (a.u.)')
+
+plt.subplot(224)
+#plt.title("Propagated state via Accurate Wigner propagator")
+plt.text(-18, -8, '(d)', color='k', fontsize=15)
+plt.imshow(acc_propagated_state, **img_params)
+plt.xlim([-20., 20])
+plt.ylim([-10., 10])
+plt.xlabel('$x$ (a.u.)')
+#plt.ylabel('$p$ (a.u.)')
+
+plt.show()
 
 
-from FastWigner.wigner_bloch_cuda1d import WignerBlochCUDA1D as FastWignerPropagator
