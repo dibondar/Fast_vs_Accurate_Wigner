d3dcf5d6187d908b9343a1315374c4cacc98a5e4
dbondar <kdfstudio@gmail.com>
2016-09-18 21:39:06 -0400

    morse potential simulations added

--- morse_potential.py
+++ morse_potential.py
@@ -1,6 +1,6 @@
 __doc__ = """
 To check accuracy of FastWigner vs AccurateWigner propagator
-using the soft core Coulomb potential as an example
+using the Morese potential as an example
 """
 
 import matplotlib.pyplot as plt
@@ -20,16 +20,36 @@ sys_params = dict(
     dt=0.01,
 
     X_gridDIM=1024,
-   # X_amplitude=30.,
+    X_amplitude=20.,
 
     P_gridDIM=1024,
-    #P_amplitude=40.,
+    # P_amplitude=20.,
 
     # the kinetic energy
     K="0.5 * P * P",
 
-    # the soft core Coulomb potential for Ar
-    V="-1. / sqrt(X * X + 1.37)",
+    # the Morse potential from Fig. 1 of http://dx.doi.org/10.1103/PhysRevLett.114.050401
+    V="15. * (exp(-0.5 * (X + 7.)) - 2. * exp(-0.25 * (X + 7.)))",
+
+    #functions="""
+    #// Blackman filter to be used as absorbing boundary condition
+    #__device__ double blackman2D(const int i, const int j)
+    #{
+    #    const double Cj = j * 2.0 * M_PI / (X_gridDIM - 1.);
+    #    const double Ci = i * 2.0 * M_PI / (P_gridDIM - 1.);
+    #
+    #    return  (0.42 - 0.5 * cos(Cj) + 0.08 * cos(2.0 * Cj)) *
+    #            (0.42 - 0.5 * cos(Ci) + 0.08 * cos(2.0 * Ci));
+    #}
+    #""",
+
+    #alpha=0.01,
+
+    # define absorbing boundary for FastWignerPropagator
+    #abs_boundary_x_p="pow(abs(blackman2D(i, j)), alpha * dt)",
+
+    # define absorbing boundary for AccurateWignerPropagator
+    #abs_boundary_x="pow(abs(blackman2D(i, j)), alpha * dt)",
 )
 
 ##########################################################################################
@@ -37,14 +57,15 @@ sys_params = dict(
 #   Propagate
 #
 ##########################################################################################
-
 import numpy as np
-acc_prop = AccurateWignerPropagator(X_amplitude=30., P_amplitude=50., **sys_params)
-acc_ground_state = acc_prop.get_ground_state(abs_tol_purity=1e-10).get().real
+# P_amplitude=np.pi*512/20.
+acc_prop = AccurateWignerPropagator(P_amplitude=50., **sys_params)
+acc_ground_state = acc_prop.get_ground_state(abs_tol_purity=1e-12).get().real
 acc_propagated_state = acc_prop.propagate(10000).get().real
+#acc_propagated_state = acc_ground_state
 
-fast_prop = FastWignerPropagator(X_amplitude=30., P_amplitude=20.,**sys_params)
-fast_ground_state = fast_prop.get_ground_state(abs_tol_purity=1e-10).get()
+fast_prop = FastWignerPropagator(P_amplitude=10., **sys_params)
+fast_ground_state = fast_prop.get_ground_state(abs_tol_purity=1e-12).get()
 fast_propagated_state = fast_prop.propagate(10000).get()
 
 ##########################################################################################
@@ -59,24 +80,24 @@ img_params = dict(
     origin='lower',
     aspect=1,
     cmap='bwr',
-    norm=WignerNormalize(vmin=-5e-9, vmax=5e-9),
+    norm=WignerNormalize(vmin=-1e-10, vmax=1e-10),
 )
 
 plt.subplot(221)
 #plt.title("Ground state Wigner function obtained via Fast Wigner propagator")
-plt.text(-18, -8, '(a)', color='k', fontsize=15)
+#plt.text(-18, -8, '(a)', color='k', fontsize=15)
 plt.imshow(fast_ground_state, **img_params)
-plt.xlim([-20., 20])
-plt.ylim([-10., 10])
+#plt.xlim([-20., 20])
+#plt.ylim([-10., 10])
 #plt.xlabel('$x$ (a.u.)')
 plt.ylabel('$p$ (a.u.)')
 
 plt.subplot(222)
 #plt.title("Propagated state via Fast Wigner propagator")
-plt.text(-18, -8, '(b)', color='k', fontsize=15)
+#plt.text(-18, -8, '(b)', color='k', fontsize=15)
 plt.imshow(fast_propagated_state, **img_params)
-plt.xlim([-20., 20])
-plt.ylim([-10., 10])
+#plt.xlim([-20., 20])
+#plt.ylim([-10., 10])
 #plt.xlabel('$x$ (a.u.)')
 #plt.ylabel('$p$ (a.u.)')
 
@@ -91,20 +112,20 @@ img_params["extent"]=[acc_prop.X.min(), acc_prop.X.max(), acc_prop.P.min(), acc_
 
 plt.subplot(223)
 #plt.title("Ground state Wigner function obtained via Accurate Wigner propagator")
-plt.text(-18, -8, '(c)', color='k', fontsize=15)
+#plt.text(-18, -8, '(c)', color='k', fontsize=15)
 plt.imshow(acc_ground_state, **img_params)
-plt.xlim([-20., 20])
-plt.ylim([-10., 10])
+#plt.xlim([-20., 20])
+#plt.ylim([-10., 10])
 plt.xlabel('$x$ (a.u.)')
 plt.ylabel('$p$ (a.u.)')
 
 plt.subplot(224)
 #plt.title("Propagated state via Accurate Wigner propagator")
-plt.text(-18, -8, '(d)', color='k', fontsize=15)
+#plt.text(-18, -8, '(d)', color='k', fontsize=15)
 plt.imshow(acc_propagated_state, **img_params)
-plt.xlim([-20., 20])
-plt.ylim([-10., 10])
-plt.xlabel('$x$ (a.u.)')
+#plt.xlim([-20., 20])
+#plt.ylim([-10., 10])
+#plt.xlabel('$x$ (a.u.)')
 #plt.ylabel('$p$ (a.u.)')
 
 plt.show()
